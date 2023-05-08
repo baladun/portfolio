ffafd4d3b2d9ec801593e7446c4cde1d0786d60a
Alexander Balan <a.balan.job@gmail.com>
2023-05-09 02:10:50 +0300

    build: Modifies tailwind.config
    - adds colors
    - adds plugins (typography, forms)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}', //
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     fontFamily: {
       montserrat: ['var(--font-montserrat)'],
       madreRose: ['var(--font-madre-rose)'],
     },
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: '#fffcfc',
+      snow: '#f9f4f2',
+      black: '#111',
+      brown: '#927070',
+      orange: {
+        50: '#fdeee0',
+        100: '#fad5b3',
+        200: '#f7b980',
+        300: '#f49d4d',
+        400: '#f18826',
+        DEFAULT: '#ef7300',
+        600: '#ed6b00',
+        700: '#eb6000',
+        800: '#e85600',
+        900: '#e44300',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'), //
+    require('@tailwindcss/forms'),
+  ],
 };
