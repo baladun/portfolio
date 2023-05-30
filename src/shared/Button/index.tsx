5c10c696266a3a14d30bb0354a57ebf8e814a795
Alexander Balan <a.balan.job@gmail.com>
2023-05-30 18:09:17 +0300

    refactor: Improves Button

--- src/shared/Button/index.tsx
+++ src/shared/Button/index.tsx
@@ -4,51 +4,59 @@ import styles from './styles.module.scss';
 import { ButtonProps } from './types';
 import classnames from 'classnames';
 import { getAppearance, getDimensions, getDisabled, getFocus } from './utils';
-import { Children, memo } from 'react';
+import { Children, forwardRef, memo } from 'react';
 
-export const Button = memo(function Button({
-  kind = 'filled', //
-  color = 'orange',
-  size = 'md',
-  icon,
-  iconPosition = 'start',
-  disabled,
-  loading,
-  type = 'button',
-  className,
-  children,
-  ...rest
-}: ButtonProps) {
-  const iconOnly = !!icon && Children.count(children) === 0;
-  const contentOnly = !icon && !!Children.count(children);
-  const iconAndContent = icon && !!Children.count(children);
+export const Button = memo(
+  forwardRef<HTMLButtonElement, ButtonProps>(
+    (
+      {
+        kind = 'filled', //
+        color = 'orange',
+        size = 'md',
+        icon,
+        iconPosition = 'start',
+        disabled,
+        loading,
+        type = 'button',
+        className,
+        children,
+        ...rest
+      },
+      ref,
+    ) => {
+      const iconOnly = !!icon && Children.count(children) === 0;
+      const contentOnly = !icon && !!Children.count(children);
+      const iconAndContent = icon && !!Children.count(children);
 
-  const classNames = classnames(
-    'rounded-[0.5em]', //
-    'font-normal',
-    getDimensions(size, kind, iconOnly),
-    getAppearance(kind, color),
-    getFocus(color),
-    disabled ? getDisabled() : '',
-    className,
-  );
+      const classNames = classnames(
+        'rounded-[0.5em]', //
+        'font-medium',
+        getDimensions(size, kind, iconOnly),
+        getAppearance(kind, color),
+        getFocus(color),
+        disabled ? getDisabled() : '',
+        className,
+      );
 
-  const iconClassNames = classnames(
-    styles.icon, //
-    iconPosition === 'start' && iconAndContent ? 'pr-[0.5em]' : '',
-    iconPosition === 'end' && iconAndContent ? 'pl-[0.5em]' : '',
-  );
+      const iconClassNames = classnames(
+        styles.icon, //
+        iconPosition === 'start' && iconAndContent ? 'pr-[0.5em]' : '',
+        iconPosition === 'end' && iconAndContent ? 'pl-[0.5em]' : '',
+      );
 
-  return (
-    <button
-      {...rest}
-      className={classNames}
-    >
-      {icon && iconPosition === 'start' && <span className={iconClassNames}>{icon}</span>}
+      return (
+        <button
+          ref={ref}
+          {...rest}
+          className={classNames}
+        >
+          {icon && iconPosition === 'start' && <span className={iconClassNames}>{icon}</span>}
 
-      {(contentOnly || iconAndContent) && <span>{children}</span>}
+          {(contentOnly || iconAndContent) && <span>{children}</span>}
 
-      {icon && iconPosition === 'end' && <span className={iconClassNames}>{icon}</span>}
-    </button>
-  );
-});
+          {icon && iconPosition === 'end' && <span className={iconClassNames}>{icon}</span>}
+        </button>
+      );
+    },
+  ),
+);
