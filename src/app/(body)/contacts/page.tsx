3fedf533a20c992e0d5b4383c46a7c4d217af47b
Alexander Balan <a.balan.job@gmail.com>
2023-09-04 02:14:09 +0200

    feat: Implements 'Contacts' section

--- src/app/(body)/contacts/page.tsx
+++ src/app/(body)/contacts/page.tsx
@@ -1,3 +1,80 @@
+import { Typography } from '@/shared/Typography';
+import { PageLayoutStatic } from '@/components/PageLayoutStatic';
+
+const { Heading, NavLinkExternal } = Typography;
+
 export default function Page() {
-  return <div>contacts</div>;
+  return (
+    <PageLayoutStatic
+      backHref="/"
+      heading={
+        <Heading
+          level={5}
+          kind="secondary"
+          color="snow"
+        >
+          Contacts
+        </Heading>
+      }
+    >
+      <div>
+        <div className="mb-4">
+          <NavLinkExternal
+            href="mailto:maxfadeev@gmail.com"
+            color="snow"
+          >
+            maxfadeev@gmail.com
+          </NavLinkExternal>
+        </div>
+
+        <div className="mb-20">
+          <NavLinkExternal
+            href="tel:375291234546"
+            color="snow"
+          >
+            +375 29 123 45 46
+          </NavLinkExternal>
+        </div>
+
+        <Heading
+          level={5}
+          kind="secondary"
+          color="snow"
+          className="mb-[0.875rem] leading-9 sm:mb-6 xl:mb-10"
+        >
+          Socials
+        </Heading>
+
+        <div className="mb-4">
+          <NavLinkExternal
+            href="https://instagram.com/fadeev_maxim"
+            target="_blank"
+            color="snow"
+          >
+            instagram.com/fadeev_maxim
+          </NavLinkExternal>
+        </div>
+
+        <div className="mb-4">
+          <NavLinkExternal
+            href="https://facebook.com/max.fadeev"
+            target="_blank"
+            color="snow"
+          >
+            facebook.com/max.fadeev
+          </NavLinkExternal>
+        </div>
+
+        <div className="mb-4">
+          <NavLinkExternal
+            href="https://vk.com/max_bezdomniy"
+            target="_blank"
+            color="snow"
+          >
+            vk.com/max_bezdomniy
+          </NavLinkExternal>
+        </div>
+      </div>
+    </PageLayoutStatic>
+  );
 }
--- src/components/Header/Socials/utils.ts
+++ src/components/Header/Socials/utils.ts
@@ -5,15 +5,15 @@ interface Config {
 
 export const socialsConfig: Config[] = [
   {
-    path: 'https://vk.com',
+    path: 'https://vk.com/max_bezdomniy',
     text: 'vk',
   },
   {
-    path: 'https://www.facebook.com',
+    path: 'https://www.facebook.com/max.fadeev',
     text: 'Fb',
   },
   {
-    path: 'https://www.instagram.com',
+    path: 'https://www.instagram.com/fadeev_maxim',
     text: 'Ig',
   },
 ];
