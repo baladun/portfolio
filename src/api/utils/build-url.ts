174273b843cbff3ca7642c585bc8945c4c8bd260
Alexander Balan <a.balan.job@gmail.com>
2023-09-15 14:35:15 +0200

    test env vars

--- src/api/utils/build-url.ts
+++ src/api/utils/build-url.ts
@@ -1,3 +1,4 @@
 export function buildUrl(path: string): string {
+  console.log(process.env.NEXT_PUBLIC_HOST, process.env.NEXT_PUBLIC_GCP_BUCKET_NAME, '!!!!!!!!!!!!!!!!!!!!!!');
   return `${process.env.NEXT_PUBLIC_HOST}/api${path}`;
 }
