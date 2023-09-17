35fec279298706523de54a7de7c43f134e317024
Alexander Balan <a.balan.job@gmail.com>
2023-09-17 02:20:16 +0200

    build: Refactors env variables

--- src/api/utils/build-url.ts
+++ src/api/utils/build-url.ts
@@ -1,4 +1,3 @@
 export function buildUrl(path: string): string {
-  console.log(process.env.NEXT_PUBLIC_HOST, process.env.NEXT_PUBLIC_GCP_BUCKET_NAME, '!!!!!!!!!!!!!!!!!!!!!!');
-  return `${process.env.NEXT_PUBLIC_HOST}/api${path}`;
+  return `${process.env.NEXT_PUBLIC_VERCEL_URL}/api${path}`;
 }
--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,14 +1,8 @@
-import { cleanEnv, email, host, port, str, url } from 'envalid';
+import { cleanEnv, email, host, str, url } from 'envalid';
 
 export function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     cleanEnv(process.env, {
-      DB_TYPE: str(),
-      DB_USER: str(),
-      DB_PASSWORD: str(),
-      DB_HOST: host(),
-      DB_PORT: port(),
-      DB_NAME: str(),
       DB_URL: url(),
       DB_PRISMA_URL: url(),
       DB_URL_NON_POOLING: url(),
@@ -18,7 +12,7 @@ export function register() {
       GCP_PRIVATE_KEY: str(),
       NEXT_PUBLIC_GCP_BUCKET_NAME: str(),
 
-      NEXT_PUBLIC_HOST: url(),
+      NEXT_PUBLIC_VERCEL_URL: url(),
 
       NEXT_PUBLIC_FIREBASE_API_KEY: str(),
       NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: host(),
